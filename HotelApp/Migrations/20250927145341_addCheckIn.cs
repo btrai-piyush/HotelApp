d643202af5048228886c9f39be18eaf4d2ac9d1d
Piyush Bhattarai <bhattarai.piyush.14@gmail.com>
2025-09-27 21:30:11 +0545

    finished data access

---
+++ HotelApp/Migrations/20250927145341_addCheckIn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelAppLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCheckIn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CheckedIn",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckedIn",
+                table: "Bookings");
+        }
+    }
+}
