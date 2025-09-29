8854b54f29647e72ae80a6e0b240efd80dd16fea
Piyush Bhattarai <bhattarai.piyush.14@gmail.com>
2025-09-29 21:59:05 +0545

    dbcontext DI

---
+++ HotelAppMVC/Controllers/RoomSearchController.cs
@@ -0,0 +1,38 @@
+﻿using HotelAppLibrary.Data;
+using HotelAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelAppMVC.Controllers
+{
+    public class RoomSearchController : Controller
+    {
+        private readonly EfDataAccess _efDataAccess;
+
+        public RoomSearchController(EfDataAccess efDataAccess)
+        {
+            _efDataAccess = efDataAccess;
+        }
+
+        public IActionResult SearchRoom()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchRoom(RoomSearchModel roomSearchModel)
+        {
+            roomSearchModel.AvailableRoomTypes = await _efDataAccess.GetAvailableRoomTypes(roomSearchModel.StartDate, roomSearchModel.EndDate);
+
+            return RedirectToAction("BookRoom", roomSearchModel);
+        }
+
+        [HttpGet]
+        public IActionResult BookRoom(RoomSearchModel roomSearchModel)
+        {
+
+            return View(roomSearchModel);
+        }
+
+
+    }
+}
