f11be5b461cc2933abd8f444952c514660182db5
Piyush Bhattarai <bhattarai.piyush.14@gmail.com>
2025-10-16 19:49:31 +0545

    added desktop app for guest check in

---
+++ HotelApp.Desktop/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using HotelAppLibrary.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ServiceProvider serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<CheckInForm>();
+            services.AddTransient<EfDataAccess>();
+
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = builder.Build();
+            services.AddDbContext<HotelAppContext>(options => options.UseSqlServer(config.GetConnectionString("Default")));
+
+            services.AddSingleton(config);
+
+            serviceProvider = services.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+
+            mainWindow.Show();
+        }
+    }
+
+}
